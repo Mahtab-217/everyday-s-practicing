7d6e020254bf506fb3b9528833af9a0f57d457ef
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-12-16 20:08:54 +0430

    new codes

--- new.js
+++ new.js
@@ -1,21 +1,21 @@
-const number="";
-if(number){
-    console.log("added to codes");
-}
-else{
-    console.log("was not added to codes")
-}
-const name=prompt("Enter your name")
-const trimed =name.trim();
-if(trimed.length>2 && trimed.length<20){
-    console.log("Your entered a valid and good name")
-}
-else{
-    console.log("You entered an unvalid and a bad name")
-}
-function sayHello(...a){
-return a.reduce((total,num)=>{
-    return total+num;
-},0);
-}
-sayHello(2,4,3,4,5,6,6,7,12,23,54);
+// const number="";
+// if(number){
+//     console.log("added to codes");
+// }
+// else{
+//     console.log("was not added to codes")
+// }
+// const name=prompt("Enter your name")
+// const trimed =name.trim();
+// if(trimed.length>2 && trimed.length<20){
+//     console.log("Your entered a valid and good name")
+// }
+// else{
+//     console.log("You entered an unvalid and a bad name")
+// }
+// function sayHello(...a){
+// return a.reduce((total,num)=>{
+//     return total+num;
+// },0);
+// }
+// sayHello(2,4,3,4,5,6,6,7,12,23,54);
