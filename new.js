5e01ff14775a6fbd8ebf088206129ad23ef7e01b
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-12-19 19:07:37 +0430

    refreshed

--- new.js
+++ new.js
@@ -29,4 +29,24 @@ else if (name==="Ahmad") {
     console.log(`We don't know you dear one`)
 }
 
-
+const number="";
+if(number){
+    console.log("added to codes");
+}
+else{
+    console.log("was not added to codes")
+}
+const name1=prompt("Enter your name")
+const trimed =name.trim();
+if(trimed.length>2 && trimed.length<20){
+    console.log("Your entered a valid and good name")
+}
+else{
+    console.log("You entered an unvalid and a bad name")
+}
+function sayHello(...a){
+return a.reduce((total,num)=>{
+    return total+num;
+},0);
+}
+sayHello(2,4,3,4,5,6,6,7,12,23,54);
