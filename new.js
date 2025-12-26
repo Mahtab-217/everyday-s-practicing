96d2b96c731e311e0c4d6097383e30fb45bfd592
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-12-26 21:19:24 +0430

    updated codes

---
+++ new.js
@@ -0,0 +1,10 @@
+const div=document.querySelector("div");
+const p=document.querySelector("div> p:first-child");
+// div.setAttribute("class",'box');
+div.className="box";
+div.className=div.className+" line";
+// div.className="line";
+// div.classList.add("box")
+// div.classList.add("line")
+p.setAttribute("class",'p');
+// div.classList.remove("line");
